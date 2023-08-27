5fc41d179ba8ed25a95cf40d188ce97b5921deea
pedromessetti <pedromessetti@gmail.com>
2023-08-27 15:15:30 +0100

    create file for parser

---
+++ src/parse.c
@@ -0,0 +1,25 @@
+// int parser(t_token *tokens, int token_count)
+// {
+// 	t_cmd_tb *cmd_list = NULL;
+// 	int i = 0;
+// 	while (i < token_count)
+// 	{
+// 		if (tokens[i].type == TOKEN_IDENTIFIER)
+// 		{
+// 			/*
+// 			Se for um comando chamar find_path e armazenar path na cmd_tb 
+// 			(mesmo q seja um comando inválido) junto dos argumentos do comando
+// 			*/
+// 		}
+// 		else if (tokens[i].type == TOKEN_OPERATOR)
+// 		{
+// 			/*
+// 			Se for | chamar add_tail
+// 			Se for < redirecionar input do comando
+// 			Se for > redirecionar output do comando
+// 			*/
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
+// }
