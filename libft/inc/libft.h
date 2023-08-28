7147634fd3ed1999685f6861a1ef499fe87a7f7c
pedromessetti <pedromessetti@gmail.com>
2023-08-28 12:16:35 +0100

    add ft_strcat

--- libft/inc/libft.h
+++ libft/inc/libft.h
@@ -68,6 +68,7 @@ char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_multi_strjoin(const char *delimiter, ...);
 size_t				ft_strlcpy(char *dst, const char *src, size_t size);
 size_t				ft_strlcat(char *dst, const char *src, size_t size);
+char				*ft_strcat(char *dest, const char *src);
 char				*ft_strchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
