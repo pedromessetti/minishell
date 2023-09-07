34c3d234d49e1a7da7e8dfeaee8fd2300084ea74
pedromessetti <pedromessetti@gmail.com>
2023-09-07 13:05:08 +0100

    trying to improve execute_cd

--- src/parse.c
+++ src/parse.c
@@ -3,64 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: annamarianunes <annamarianunes@student.    +#+  +:+       +#+        */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 13:26:50 by annamarianu       #+#    #+#             */
-/*   Updated: 2023/09/06 11:15:52 by annamarianu      ###   ########.fr       */
+/*   Updated: 2023/09/07 11:36:14 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int parse(t_token *tokens, int token_count, char **envp)
- {
-    t_cmd_tb *cmd_list = NULL;
-    int i = 0;
-
-    while (i < token_count) 
-    {
-        if (tokens[i].type == TOKEN_KEYWORD && strcmp(tokens[i].content, "cd") == 0)
-        {
-            i++;
-            execute_cd(tokens[i].content);
-        }
-        else if (tokens[i].type == TOKEN_KEYWORD && strcmp(tokens[i].content, "echo") == 0) 
-        {
-            i++;
-            const char **echo_args = (const char **)malloc((token_count - i + 1) * sizeof(const char *));
-            int arg_index = 0;
-            if (!echo_args)
-                return -1;
-            while (i < token_count) 
-            {
-                if (tokens[i].type == TOKEN_OPERATOR && strcmp(tokens[i].content, "|") == 0) 
-                    break; 
-                echo_args[arg_index] = tokens[i].content;
-                arg_index++;
-                i++;
-            }
-            echo_args[arg_index] = NULL;
-            execute_echo(echo_args);
-            free(echo_args);
-        } 
-        else if (tokens[i].type == TOKEN_KEYWORD || tokens[i].type == TOKEN_IDENTIFIER)
-            i = execute_identifier(tokens, token_count, envp, i, &cmd_list);
-        else if (tokens[i].type == TOKEN_OPERATOR && ft_strcmp(tokens[i].content, "|") == 0) 
-        {
-            i++;
-            continue;
-        }
-        i++;
-    }
-    start_process(cmd_list, envp);
-    free_path_list(&cmd_list);
-    return 0;
-}
-
-
 void execute_cd(const char *directory) {
-    if (chdir(directory) != 0 ) {
-        perror("cd");
+    if (!*directory){
+        chdir(ft_getenv("HOME"));
+    }
+    else
+    {
+        if(chdir(directory) != 0 ) 
+            perror("minishell: cd");
     }
 }
 
@@ -123,6 +82,101 @@ int execute_identifier(t_token *tokens, int token_count, char **envp, int start,
         i++;
     }
     return i;
+}
+
+int parse(t_token *tokens, int token_count, char **envp)
+ {
+    t_cmd_tb *cmd_list = NULL;
+    int i = 0;
+
+    while (i < token_count) 
+    {
+        if (ft_strcmp(tokens[i].content, "cd") == 0)
+        {
+            i++;
+            if (ft_strcmp(tokens[i+1].content, "|") == 0)
+                i++;
+            else if (ft_strcmp(tokens[i].content, "|") == 0)
+                continue;
+            else
+                execute_cd(tokens[i].content);
+        }
+        // else if (ft_strncmp(tokens[i].content, "unset", 5) == 0)
+        // {
+        //     i++;
+        //     while (i < token_count && tokens[i].type == TOKEN_ARG) 
+        //     {
+        //         unsetenv(tokens[i].content);
+        //         i++;
+        //     }
+        // }
+        // else if (ft_strncmp(tokens[i].content, "export", 6) == 0) 
+        // {
+        //     i++;
+        //     while (i < token_count && tokens[i].type == TOKEN_ARG) 
+        //     {
+        //         char *env_var = ft_strdup(tokens[i].content);
+        //         char *env_var_value = ft_strchr(env_var, '=');
+        //         if (env_var_value) 
+        //         {
+        //             *env_var_value = '\0';
+        //             env_var_value++;
+        //             setenv(env_var, env_var_value, 1);
+        //         }
+        //         else 
+        //         {
+        //             setenv(env_var, "", 1);
+        //         }
+        //         free(env_var);
+        //         i++;
+        //     }
+        // }
+        else if (ft_strncmp(tokens[i].content, "env", 3) == 0) 
+        {
+            int fd = open(".env", O_RDONLY);
+            // printf(":%s:\n", tokens[i+1].content);
+            if (tokens[i+1].content) {
+                char *line = get_next_line(fd);
+                while (line) 
+                {
+                    printf("%s", line);
+                    line = get_next_line(fd);
+                }
+            }
+            close(fd);
+            i++;
+        }
+        else if (tokens[i].type == TOKEN_KEYWORD && strcmp(tokens[i].content, "echo") == 0) 
+        {
+            i++;
+            const char **echo_args = (const char **)malloc((token_count - i + 1) * sizeof(const char *));
+            int arg_index = 0;
+            if (!echo_args)
+                return -1;
+            while (i < token_count) 
+            {
+                if (tokens[i].type == TOKEN_OPERATOR && strcmp(tokens[i].content, "|") == 0) 
+                    break; 
+                echo_args[arg_index] = tokens[i].content;
+                arg_index++;
+                i++;
+            }
+            echo_args[arg_index] = NULL;
+            execute_echo(echo_args);
+            free(echo_args);
+        } 
+        else if (tokens[i].type == TOKEN_IDENTIFIER)
+            i = execute_identifier(tokens, token_count, envp, i, &cmd_list);
+        else if (tokens[i].type == TOKEN_OPERATOR && ft_strcmp(tokens[i].content, "|") == 0) 
+        {
+            i++;
+            continue;
+        }
+        i++;
+    }
+    start_process(cmd_list, envp);
+    free_path_list(&cmd_list);
+    return 0;
 }
 
 // int parse_key_literal(t_token *tokens, int token_count, char **envp, int start, t_cmd_tb **cmd_list) 
