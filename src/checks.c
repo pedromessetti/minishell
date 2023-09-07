d994afdb344a504ffe3f8058e655319cbdbfa957
pedromessetti <pedromessetti@gmail.com>
2023-09-08 00:59:19 +0100

    rm unecessary

--- src/checks.c
+++ src/checks.c
@@ -21,42 +21,6 @@ void	check_ac(int ac)
 		exit(EXIT_FAILURE);
 }
 
-// int	*check_fd(int fd[], char **av, int ac)
-// {
-// 	int	flags;
-
-// 	flags = O_CREAT | O_WRONLY | O_TRUNC;
-// 	if (is_here_doc(av[1]))
-// 		flags = O_CREAT | O_WRONLY | O_APPEND;
-// 	fd[1] = open(av[ac - 1], flags, 0644);
-// 	if (fd[1] == -1)
-// 	{
-// 		ft_printf("minishell: Error opening/creating the file\n");
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	if (!is_here_doc(av[1]))
-// 	{
-// 		fd[0] = open(av[1], O_RDONLY);
-// 		if (fd[0] == -1)
-// 		{
-// 			perror(av[1]);
-// 			exit(EXIT_FAILURE);
-// 		}
-// 	}
-// 	return (fd);
-// }
-
-// int	check_builtin(char *av)
-// {
-// 	if (ft_strncmp(av, "exit\0", ft_strlen("exit") + 1) == 0 || ft_strncmp(av,
-// 			"exit ", ft_strlen("exit ")) == 0)
-// 		return (1);
-// 	if (ft_strncmp(av, "cd\0", ft_strlen("cd") + 1) == 0 || ft_strncmp(av,
-// 			"cd ", ft_strlen("cd ")) == 0)
-// 		return (1);
-// 	return (0);
-// }
-
 void	check_exit_status(char *arg)
 {
 	if (ft_str_is_num(arg))
@@ -64,21 +28,6 @@ void	check_exit_status(char *arg)
 	else
 		ft_printf("minishell: exit: %s: numeric argument required\n", arg);
 }
-
-// void	check_env(char **envp)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (envp[i])
-// 	{
-// 		if (ft_strncmp(envp[i], "PATH=", ft_strlen("PATH=")) == 0)
-// 			return ;
-// 		i++;
-// 	}
-// 	ft_printf("minishell: PATH not set\n");
-// 	exit(EXIT_FAILURE);
-// }
 
 // int	ask_for_exit_status(char **args)
 // {
