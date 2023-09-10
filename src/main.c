d52000d4d898374bf0515c7e6cc4ddff8ec0af44
pedromessetti <pedromessetti@gmail.com>
2023-09-10 23:10:50 +0100

    add signals

--- src/main.c
+++ src/main.c
@@ -6,20 +6,21 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 09:11:30 by pedro             #+#    #+#             */
-/*   Updated: 2023/09/08 00:41:35 by pedro            ###   ########.fr       */
+/*   Updated: 2023/09/09 21:28:58 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	init(char **envp)
+void	init(t_env **env)
 {
 	char	*prompt;
 
-	create_env(envp);
 	while (1)
 	{
 		prompt = readline("minishell> ");
+		if (!prompt)
+			exit(EXIT_SUCCESS);
 		add_history(prompt);
 		if (ft_strnstr(prompt, "exit", ft_strlen(prompt)))
 		{
@@ -28,18 +29,28 @@ void	init(char **envp)
 			while (*prompt == ' ')
 				prompt++;
 			check_exit_status(prompt);
+			free_env(env);
 			printf("exit\n");
 			exit(EXIT_SUCCESS);
 		}
-		lex(prompt);
+		lex(prompt, env);
 		free(prompt);
 	}
 }
 
 int	main(int ac, char **av, char **envp)
 {
+	t_env	*env;
+
+	env = NULL;
 	(void)av;
 	check_ac(ac);
-	init(envp);
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, SIG_IGN);
+	int i = -1;
+	while (envp[++i])
+		env = create_env(env, envp[i]);
+	init(&env);
+	free_env(&env);
 	return (0);
 }
---
+++ src/sig.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sig.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 17:45:56 by pedro             #+#    #+#             */
+/*   Updated: 2023/09/09 17:46:15 by pedro            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	signal_handler(int sig)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = waitpid(-1, &status, 0);
+	if (sig == SIGINT)
+	{
+		if (pid == -1)
+		{
+			write(1, "\n", 1);
+			rl_replace_line("", 0);
+			rl_on_new_line();
+			rl_redisplay();
+		}
+		else
+			write(1, "\n", 1);
+	}
+	if (sig == SIGQUIT)
+		write(1, "\n", 1);
+}
